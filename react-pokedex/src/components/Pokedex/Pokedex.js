8bc928f90137b923832cff5f55d69ea95c107055
Brandon Kaufhold <brandonkaufhold@gmail.com>
2018-02-09 02:52:17 -0500

    Added basic type button rendering functionality, poke-specific image display, tidied up some styles and fixed a component bug I found

--- react-pokedex/src/components/Pokedex/Pokedex.js
+++ react-pokedex/src/components/Pokedex/Pokedex.js
@@ -8,60 +8,71 @@ function Header(props) {
   </div>);
 }
 
-
-function PokeImg() {
+function PokeImg(props) {
   return (<div className="seven columns">
-    <img className="poke-img-main" src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png" alt="pokemon"/>
+    <img className="poke-img-main" src={props.pokeImg} alt="pokemon"/>
   </div>);
 }
 
-class PokeInfo extends Component {
-  render(props) {
+function TypesListItem(props) {
+  console.log('Hey: ' + props.value);
+  return (
+    <li>
+    <div>
+      <h6>{props.value}</h6>
+    </div>
+  </li>
+);
+}
 
-    return (<div className="five columns">
-      <div className="row poke-name-con">
-        <div className="row poke-name">
+function TypeList(props) {
+  const pokemonTypes = props.pokeTypes;
+  console.log(pokemonTypes);
+  const pokeTypesList = pokemonTypes.map((typeI) =>
+  <TypesListItem key={typeI.type.name} value={typeI.type.name}/>
+  );
+  return (<ul>{pokeTypesList}</ul>);
+}
 
-          <img src="http://pixelartmaker.com/art/797ff81281c7a32.png" alt="pixel_poke"/>
-          <h6>
-            <span>#{this.props.pokeId}</span>
-            {this.props.pokeName.toUpperCase()}</h6>
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
 
-        </div>
-        <div className="row poke-alt-name">
-          <h6>Entrancing Pokemon</h6>
-        </div>
+function PokeInfo(props) {
+  return (<div className="five columns">
+    <div className="row poke-name-con">
+      <div className="row poke-name">
+
+        <img src="http://pixelartmaker.com/art/797ff81281c7a32.png" alt="pixel_poke"/>
+        <h6 style={{float: 'left'}}>#{props.pokeId}</h6>
+        <h6>{capitalizeFirstLetter(props.pokeName)}</h6>
       </div>
-      <div className="row poke-type-con">
-        <div>
-          <h6>POISON</h6>
-        </div>
-        <div>
-          <h6>ELECTR</h6>
-        </div>
+      <div className="row poke-alt-name">
+        <h6>Entrancing Pokemon</h6>
       </div>
-      <div className="row poke-body-con">
-        <div className="row">
-          <span>HT</span>
-          <h6>5&apos;11&quot;</h6>
-        </div>
-        <div className="row">
-          <span>WT</span>
-          <h6>22.5lbs</h6>
-        </div>
+    </div>
+    <div className="row poke-type-con">
+      <TypeList pokeTypes={props.pokeTypes}/>
+    </div>
+    <div className="row poke-body-con">
+      <div className="row">
+        <span>HT</span>
+        <h6>{props.pokeHeight}ft</h6>
       </div>
-    </div>);
-  }
+      <div className="row">
+        <span>WT</span>
+        <h6>{props.pokeWeight}lbs</h6>
+      </div>
+    </div>
+  </div>);
 
 }
 
-class PokeDesc extends Component {
-  render(props) {
-    return (<div className="row">
-      <h2>Pokemon description</h2>
-      <p>{this.props.pokeDesc}</p>
-    </div>);
-  }
+function PokeDesc(props) {
+  return (<div className="row">
+    <h2>Pokemon description</h2>
+    <p>{props.pokeDesc}</p>
+  </div>);
 }
 
 class PokeInput extends Component {
@@ -72,21 +83,26 @@ class PokeInput extends Component {
       inputValue: ''
     };
 
-    //this.handleChange = this.handleChange.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleEnter = this.handleEnter.bind(this);
   }
-/*
+
   handleChange(e) {
-    this.setState({value: e.target.value});
-
-    this.props.handler();
-
-
+    this.setState({inputValue: e.target.value});
   }
-*/
+
+  handleEnter(e) {
+    if (e.key === 'Enter') {
+      this.props.updatePokemon(e);
+      console.log('Did it');
+    } else {
+      console.log('nah');
+    }
+  }
+
   render(props) {
     return (<div className="row">
-      <input className="u-full-width" type="text" value={this.state.inputValue} onChange={this.handleChange} placeholder={this.props.pokeName}/>
-      <button value="squirtle" onClick = {this.props.handler}>Hey</button>
+      <input className="u-full-width" type="text" onChange={this.handleChange} onKeyUp={this.handleEnter} placeholder="Pokemon name or #"/>
     </div>);
   }
 }
@@ -95,79 +111,87 @@ export default class Pokedex extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      pokeUrlGeneral: 'http://pokeapi.salestock.net/api/v2/pokemon/' + 1 + '/',
-      pokeUrlSpecific: 'http://pokeapi.salestock.net/api/v2/pokemon-species/' + 1 + '/',
+      pokeUrlGeneral: 'http://pokeapi.salestock.net/api/v2/pokemon/1/',
+      pokeUrlSpecific: 'http://pokeapi.salestock.net/api/v2/pokemon-species/1/',
       pokemonId: '',
       pokemonName: '',
       pokemonWeight: '',
       pokemonHeight: '',
       pokemonDesc: '... Loading',
+      pokemonTypes: []
     };
 
-    this.handler = this.handler.bind(this);
+    this.updatePokemon = this.updatePokemon.bind(this);
 
     this.requestAPI();
   }
 
   requestAPI() {
+    console.log('API request');
     fetch(this.state.pokeUrlGeneral).then(results => {
       return results.json();
     }).then(data => {
-      console.log(data);
-      this.setState({
-        pokemonName: data.name,
-        pokemonId: data.id,
-        pokemonWeight: data.weight,
-        pokemonHeight: data.height
-      });
+      const pokeArrayNew = [];
+
+
+
+      this.setState({pokemonName: data.name, pokemonId: data.id, pokemonWeight: data.weight, pokemonHeight: data.height, pokemonTypes: data.types});
+
+      console.log('General', data);
     });
 
     fetch(this.state.pokeUrlSpecific).then(results => {
       return results.json();
     }).then(data => {
-      this.setState({
-        pokemonDesc: data.flavor_text_entries[1].flavor_text,
-      });
+      this.setState({pokemonDesc: data.flavor_text_entries[1].flavor_text});
+      console.log('Specific', data);
     });
   }
 
-  parseUrlGeneral(id) {
-    return 'http://pokeapi.salestock.net/api/v2/pokemon/' + id + '/';
+  parseUrl(id, type) {
+    if (type === 'general') {
+      return 'http://pokeapi.salestock.net/api/v2/pokemon/' + id + '/';
+    } else if (type === 'specific') {
+      return 'http://pokeapi.salestock.net/api/v2/pokemon-species/' + id + '/';
+    } else {
+      console.log('Not a right id');
+    }
   }
 
-  parseUrlSpecific(id) {
-    return 'http://pokeapi.salestock.net/api/v2/pokemon-species/' + id + '/';
-  }
-
-  handler(e) {
+  updatePokemon(e) {
     e.preventDefault();
-
+    console.log('Called updatePokemon()');
     let input = e.target.value;
     this.setState({
-      pokeUrlGeneral: this.parseUrlGeneral(input),
-      pokeUrlSpecific: this.parseUrlSpecific(input),
+      pokeUrlGeneral: this.parseUrl(input, 'general'),
+      pokeUrlSpecific: this.parseUrl(input, 'specific')
     });
-
     this.requestAPI();
-    console.log(this.state.pokemonName);
   }
   render(props) {
-    const pokemon = this.state.pokemonData;
     const pokeName = this.state.pokemonName;
     const pokeId = this.state.pokemonId;
+    const pokeWeight = this.state.pokemonWeight;
+    const pokeHeight = this.state.pokemonHeight;
     const pokeDesc = this.state.pokemonDesc;
-
-    //console.log(pokemon);
+    const pokeTypes = this.state.pokemonTypes;
+    const pokeImg = 'https://img.pokemondb.net/artwork/' + pokeName + '.jpg';
 
     return (<div className="container pokedex">
-      <Header />
-        <div className="row">
-          <PokeImg />
-          <PokeInfo pokeName={pokeName} pokeId={pokeId} />
-        </div>
-      <PokeDesc pokeDesc={pokeDesc}/>
-      <PokeInput handler={this.handler}  pokeName={pokeName}/>
-
+      {
+        pokeName
+          ? (<div>
+            <Header/>
+            <div className="row">
+              <PokeImg pokeImg={pokeImg}/>
+              <PokeInfo pokeName={pokeName} pokeId={pokeId} pokeTypes={pokeTypes} pokeWeight={pokeWeight} pokeHeight={pokeHeight}/>
+            </div>
+            <PokeDesc pokeDesc={pokeDesc}/>
+            <PokeInput updatePokemon={this.updatePokemon} pokeName={pokeName}/>
+          </div>)
+          : (<h2>Loading Pokemon...</h2>)
+      }
     </div>);
+
   }
 }
